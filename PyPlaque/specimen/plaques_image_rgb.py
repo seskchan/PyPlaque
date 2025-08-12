73ecc5faa37666202f186c49bce3f3dcb66198d9
seskchan <suiki.chan@sanofi.com>
2025-08-12 11:03:13 -0400

    add unit test module

--- PyPlaque/specimen/plaques_image_rgb.py
+++ PyPlaque/specimen/plaques_image_rgb.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from PyPlaque.specimen import PlaquesMask
-
+from ..specimen import PlaquesMask
+from ..utils import fixed_threshold
 
 class PlaquesImageRGB(PlaquesMask):
   """
@@ -28,16 +28,36 @@ class PlaquesImageRGB(PlaquesMask):
   def __init__(self, 
                name: str, 
                image: np.ndarray, 
-               plaques_mask: np.ndarray, 
+               plaques_mask: np.ndarray | None = None, 
+               threshold = None,
+               sigma = 5,
                use_picks:bool=False):
     # check types
     if not isinstance(name, str):
       raise TypeError("Image name atribute must be a str")
     if (not isinstance(image, np.ndarray)) or (not image.ndim == 3):
       raise TypeError("Image atribute must be a 3D (RGB) numpy array")
-    if (not isinstance(plaques_mask, np.ndarray)) or (not plaques_mask.ndim
-    == 2):
-      raise TypeError("Mask atribute must be a 2D numpy array")
+    if plaques_mask:
+      if (not isinstance(plaques_mask, np.ndarray)) or (not plaques_mask.ndim
+      == 2):
+        raise TypeError("Mask atribute must be a 2D numpy array")
+      self.plaques_mask = plaques_mask
+    elif threshold and sigma:
+      plaques_mask = fixed_threshold(image, threshold, sigma) # mask:RGB(x,y,3) #gaussian()/normalisation implemented along each axis
+      # Compression
+      # Avg Pooling
 
+      self.plaques_mask = plaques_mask
+    else:
+      raise ValueError("Either mask or fixed threshold must be provided")
+
+    # inherit super class (plaques_mask) __init__ with child name, plaques_mask and use_picks
     super(PlaquesImageRGB, self).__init__(name, plaques_mask,use_picks)
+    #super().__init__(name, plaques_mask, use_picks) in python3
     self.image = image
+
+if __name__ == __name__:
+  test = np.random.rand(256,256,3)
+  test_obj = PlaquesImageRGB(name="test",image=test, sigma=1)
+  print(f"original: {test}")
+  print(f"new: {test_obj.plaques_mask}")
