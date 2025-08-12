8e57f925f751ab5133572b18095a9aa86f56b462
seskchan <suiki.chan@sanofi.com>
2025-08-12 11:03:03 -0400

    test prep

--- PyPlaque/experiment/__init__.py
+++ PyPlaque/experiment/__init__.py
@@ -3,6 +3,6 @@ from PyPlaque.experiment.crystal_violet import *
 from PyPlaque.experiment.fluorescence_microscopy import *
 from PyPlaque.experiment.stain_agnostic import *
 """
-from .crystal_violet import *
-from .fluorescence_microscopy import *
-from .stain_agnostic import *
+from experiment.crystal_violet import *
+from experiment.fluorescence_microscopy import *
+from experiment.stain_agnostic import *
